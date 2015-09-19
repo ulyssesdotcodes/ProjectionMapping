5191a9baa58f6b52b26f1397217ab67e45d19afa
Ulysses Popple <ulysses.popple@gmail.com>
2015-09-19 00:26:29 -0400

    Headway

--- src/ProjectionMappingApp.cpp
+++ src/ProjectionMappingApp.cpp
@@ -9,17 +9,58 @@ using namespace std;
 class ProjectionMappingApp : public App {
   public:
 	void setup() override;
+	void keyDown(KeyEvent event);
 	void mouseDown( MouseEvent event ) override;
+	void mouseDrag( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+
+private:
+	array<vec2, 4> mVertices;
+	vec2* mSelectedVertex;
+
+	gl::VboMeshRef mVbo;
+
+	gl::BatchRef mDrawVertex;
+	gl::BatchRef mDrawRect;
 };
 
 void ProjectionMappingApp::setup()
 {
+	mVertices[0] = ivec2(100, 100);
+	mVertices[1] = ivec2(500, 100);
+	mVertices[2] = ivec2(100, 500);
+	mVertices[3] = ivec2(500, 500);
+
+	mDrawVertex = gl::Batch::create(geom::Circle().center(vec2(100, 100)).radius(50), gl::getStockShader(gl::ShaderDef().color()));
+};
+
+void ProjectionMappingApp::keyDown(KeyEvent event) {
+	if (event.getChar() == 'q') {
+		quit();
+	}
 }
 
 void ProjectionMappingApp::mouseDown( MouseEvent event )
 {
+	float dist = glm::length((vec2) event.getPos() - mVertices[0]);
+	float distIndex = 0;
+
+	// Calculate the closest point
+	for (int i = 1; i < mVertices.size(); ++i) {
+		float newDist = glm::length((vec2) event.getPos() - mVertices[i]);
+		if (newDist < dist) {
+			dist = newDist;
+			distIndex = i;
+		}
+	}
+
+	mSelectedVertex = &mVertices[distIndex];
+}
+
+void ProjectionMappingApp::mouseDrag(MouseEvent event)
+{
+	*mSelectedVertex = event.getPos();
 }
 
 void ProjectionMappingApp::update()
@@ -29,6 +70,15 @@ void ProjectionMappingApp::update()
 void ProjectionMappingApp::draw()
 {
 	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::ScopedColor color(1, 0, 0);
+
+	for (int i = 0; i < mVertices.size(); ++i) {
+		gl::ScopedModelMatrix scpModelMatrix;
+		gl::translate(mVertices[i]);
+		mDrawVertex->draw();
+	}
 }
 
-CINDER_APP( ProjectionMappingApp, RendererGl )
+CINDER_APP(ProjectionMappingApp, RendererGl(), [&](App::Settings *settings) {
+	settings->setFullScreen(true);
+})
